0ca14a28f3c3c6312356c393726f1883b44cbc6f
Vinayak Yuvraj Bhatt <vinayakybhatt@gmail.com>
2020-05-15 09:39:04 +0530

    env patch

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
-export const API_KEY = "AIzaSyAu06KqC48ESrwpmW-HyxwaFMQC1p8ECMM";
-export const GOOGLE_API = "AIzaSyB3H83Sh3E5D3qksbidR_z5zTmvdw3TRLE";
-export const ALLREQUESTS = "https://reactnative-foodwasteapp/allRequests.json";
-export const BOOKING = "https://reactnative-foodwasteapp/booking.json";
+export const API_KEY = "";
+export const GOOGLE_API = "";
+export const ALLREQUESTS = "";
+export const BOOKING = "";
 
 export const REGISTERURL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
 export const LOGINURL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
