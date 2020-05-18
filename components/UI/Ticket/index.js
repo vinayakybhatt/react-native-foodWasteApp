ac40c6abe0bfa85cf177465e7ed898d2faded5e2
Vinayak Yuvraj Bhatt <vinayakybhatt@gmail.com>
2020-05-18 09:45:34 +0530

    css tweak

--- components/UI/Ticket/index.js
+++ components/UI/Ticket/index.js
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
     elevation: 2,
     borderRadius: 5,
     borderLeftColor: Colors.primary,
-    borderRightColor: Colors.danger,
+    borderRightColor: Colors.primary,
     borderLeftWidth: 5,
     borderRightWidth: 5,
   },
